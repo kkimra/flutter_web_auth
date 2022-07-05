caadf5e28599340b63dc760d974cdaa73920f4af
Sergey <seregabozhi@gmail.com>
2022-07-05 12:19:08 +0400

    fix for flutter analyze

--- example/lib/generated_plugin_registrant.dart
+++ example/lib/generated_plugin_registrant.dart
@@ -6,7 +6,7 @@
 // ignore_for_file: lines_longer_than_80_chars
 // ignore_for_file: depend_on_referenced_packages
 
-import 'package:flutter_web_auth/src/flutter_web_auth_web.dart';
+import 'package:flutter_web_auth/flutter_web_auth_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
